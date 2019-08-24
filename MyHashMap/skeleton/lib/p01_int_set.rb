53451cc89a887eef9ee5f3ff5e3909a2d330b53d
leafhouse <wilson@wilsons-MacBook-Air.local>
2019-08-24 02:01:18 -0400

    Implemented IntSet

--- MyHashMap/skeleton/lib/p01_int_set.rb
+++ MyHashMap/skeleton/lib/p01_int_set.rb
@@ -1,7 +1,7 @@
 class MaxIntSet
 
   attr_reader :store
-  
+
   def initialize(max)
     @store = Array.new(max, false)
   end
@@ -46,22 +46,38 @@ end
 
 
 class IntSet
+  attr_reader :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+
+    if include?(num)
+      return false
+    end
+
+    self[num] << num
+
   end
 
   def remove(num)
+
+    self[num].delete(num)
+
   end
 
   def include?(num)
+
+    self[num].include?(num)
+    
   end
 
   private
 
   def [](num)
+    @store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
