1ee52eafe499e30418f39858743a37264612199e
leafhouse <wilson@wilsons-MacBook-Air.local>
2019-08-24 01:48:18 -0400

    Implemented MaxIntSet

---
+++ MyHashMap/skeleton/lib/p01_int_set.rb
@@ -0,0 +1,102 @@
+class MaxIntSet
+
+  attr_reader :store
+  
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    validate!(num)
+    return false if @store[num]
+    self.store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    if !include?(num)
+      return nil
+    else
+      self.store[num] = false
+    end
+    num
+  end
+
+  def include?(num)
+    validate!(num)
+    return self.store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    if num.between?(0, @store.length - 1)
+      true
+    else
+      false
+    end
+  end
+
+  def validate!(num)
+    if !is_valid?(num)
+      raise "Out of bounds"
+    end
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
