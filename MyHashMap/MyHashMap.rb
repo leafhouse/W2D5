65e3796c6565cc4f4c0281b02b59eb3f7cb78d3e
leafhouse <wilson@wilsons-MacBook-Air.local>
2019-08-24 01:28:44 -0400

    MyHashMap project initial commit

---
+++ MyHashMap/MyHashMap.rb
@@ -0,0 +1,20 @@
+class MaxIntSet
+	attr_reader :max, :store
+
+	def initialize(max)
+		@max = max
+		@store = Array.new(max)
+	end
+
+	def insert(num)
+
+	end
+
+	def remove(num)
+
+	end
+
+	def include?(num)
+		
+	end
+end
